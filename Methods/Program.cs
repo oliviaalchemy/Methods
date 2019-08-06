89b2cd8f0b80e72b524823e171ff39abffd7c03c
oliviaalchemy <olivia@alchemytechs.com>
2019-08-06 15:36:51 +0100

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Mike", 33);
+            SayHi("John", 56);
+            SayHi("Tom", 12);
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age) 
+        {
+            Console.WriteLine("Hello " + name + " you are " + age); 
+
+        }
+    }
+}
+
+
+
